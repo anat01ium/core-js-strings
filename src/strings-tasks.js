256b132a62ea5afa46472dd349725317773eadb3
Anatoly Sizyakin <rx.anat01ium@gmail.com>
2024-12-16 17:46:39 +0300

    feat: add implementation of the invertCase function

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -381,8 +381,13 @@ function reverseWords(str) {
  *   invertCase('JavaScript is Fun') => 'jAVAsCRIPT IS fUN'
  *   invertCase('12345') => '12345'
  */
-function invertCase(/* str */) {
-  throw new Error('Not implemented');
+function invertCase(str) {
+  return str
+    .split('')
+    .map((char) =>
+      /[A-Z]/.test(char) ? char.toLowerCase() : char.toUpperCase()
+    )
+    .join('');
 }
 
 /**
